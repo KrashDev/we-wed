1fd14afdc23178a032a65c2004f4ef0ede0e55cb
Donny Gabai <mrdgabai@gmail.com>
2017-12-12 20:38:37 -0500

    third

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-
 var deadline = 'May 5 2018 23:59:59 GMT+0200';
 
 function getTimeRemaining(endtime){
@@ -19,23 +17,16 @@ function getTimeRemaining(endtime){
 
   function initializeClock(id, endtime){
     var clock = document.getElementById(id);
-    var days = document.getElementById(id);
-    var hours = document.getElementById(id);
-    var minutes = document.getElementById(id);
-    var seconds = document.getElementById(id);
     var timeinterval = setInterval(function(){
       var t = getTimeRemaining(endtime);
-      days.innerHTML = t.days;
-      hours.innerHTML = t.hours;
-      minutes.innerHTML = t.minutes;
-      seconds.innerHTML = t.seconds;
-
+      clock.innerHTML = 'days: ' + t.days + '<br>' +
+                        'hours: '+ t.hours + '<br>' +
+                        'minutes: ' + t.minutes + '<br>' +
+                        'seconds: ' + t.seconds;
       if(t.total<=0){
         clearInterval(timeinterval);
       }
     },1000);
   }
 
-//   initializeClock('clockdiv', deadline);
-
-});
+  initializeClock('clockdiv', deadline);
